81f24d360dd1a97b3c694d09a319be6b1155e588
yeasinopu17 <yeasinopu@gmail.com>
2021-12-21 17:52:19 +0600

    com 343 and mongodb insert

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,3 +1,4 @@
+// domain/meetupId
 
 import MeetupDetail from '../../components/meetups/MeetupDetails';
 
@@ -13,4 +14,40 @@ const MeetupDetails = (props) => {
         />
     );
 };
+
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1',
+                },
+            },
+            {
+                params: {
+                    meetupId: 'm2',
+                },
+            },
+        ],
+    };
+}
+
+export async function getStaticProps(context) {
+    const meetupId = context.params.meetupId;
+    console.log(meetupId);
+
+    return {
+        props: {
+            meetupData: {
+                id: meetupId,
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
+                title: 'this is title',
+                address: '37/1 Khilgaon 1219, Dhaka',
+                discription: 'Degree finds ere scape hellas from name, and all to.',
+            },
+        },
+    };
+}
+
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,17 @@ function Homepage(props) {
     return <MeetupList meetups={props.meetups} />;
 }
 
+// export async function getServerSideProps(context) {
+//     const req = context.req;
+//     const res = context.res;
+
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS,
+//         },
+//     };
+// }
+
 export async function getStaticProps() {
     return {
         props: {
